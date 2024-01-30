b85e2a9b8899b6b65b7acc7d2f7282a137be73f8
OLHA55 <bagira2310@gmail.com>
2024-01-31 00:29:34 +0200

    js02new2

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,10 @@
 function formatMessage(message, maxLength) {
-    
+   
+    if (message.length <= maxLength) {
+        return message ;
+    } else {
+        return message.slice(0,maxLength) + `...`;
+    }
 }
 
 
